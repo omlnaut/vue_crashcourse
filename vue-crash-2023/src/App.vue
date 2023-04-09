2c792ca7457001f4023c26f7f837e795b5187ced
omlnaut <oneironaut.oml@gmail.com>
2023-04-09 21:03:37 +0200

    wip task list

--- vue-crash-2023/src/App.vue
+++ vue-crash-2023/src/App.vue
@@ -1,9 +1,48 @@
-<script setup>
+<script>
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created(){
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false
+      }
+    ]
+  }
+}
+
 </script>
 
 <template>
   <div class="container">
-    <h1>Task Tracker</h1>
+    <Header title="Task Tracker"/>
+    <Tasks :tasks="tasks"/>
     </div>
 
 </template>
